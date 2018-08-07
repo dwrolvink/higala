bb03093d4680b541b0bf07e0622fdd825d0de4a9
X1Zeth2X <zethiscool11@gmail.com>
2018-08-07 23:50:26 +0000

    Added more of the features.

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,12 +1,33 @@
 <template>
-  <v-card>
-    <v-card-title class="title font-weight-light">
-        {{ post.creator_name }}
+  <v-card class="mb-4 elevation-3">
+    <v-card-title primary-title>
+      <img class="mr-3" :src="post.usr_image" style="max-height: 40px;">
+        <h3 class="title font-weight-light"> {{ post.creator_name }} </h3>
         <v-spacer></v-spacer>
         <v-btn flat icon small color="deep-purple lighten-2">
             <v-icon>more_vert</v-icon>
         </v-btn>
+        <br/>
     </v-card-title>
+    <v-card-title>
+      <p class="subheading">{{ post.content }}</p>
+    </v-card-title>
+    <v-card-actions>
+      <div class="ml-1 mt-1 grey--text">{{ post.likes }}</div>
+      <v-btn 
+        icon 
+        flat 
+        :color="[ postLiked? 'blue': 'grey']"
+        @click="toggleLike"
+      >
+        <v-icon>thumb_up</v-icon>
+      </v-btn>
+      <v-btn icon flat color="grey">
+        <v-icon>forum</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <h3 class="subheading font-weight-light"> {{ post.created }} </h3>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -15,9 +36,24 @@ export default {
   name: 'Post',
   data() {
     return {
+        postLiked: false,
         post: {
-            creator_name: 'Ajeet Kumar'
+            creator_name: 'Zeth Leonardo',
+            usr_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Antu_distributor-logo-gentoo.svg/1024px-Antu_distributor-logo-gentoo.svg.png',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in sem quis dolor vulputate sollicitudin. Pellentesque eu felis nunc. Aenean bibendum risus tempor, posuere risus porttitor, facilisis metus. Proin blandit, turpis eget suscipit facilisis, mi lorem laoreet lorem, et placerat nibh velit ut tortor.',
+            created: 'Aug 4th, 9:32 PM',
+            likes: 302,
         }
+    }
+  },
+  methods: {
+    toggleLike() {
+      this.postLiked = !this.postLiked
+      if(this.postLiked == true) {
+        this.post.likes = this.post.likes + 1
+      } else {
+        this.post.likes = this.post.likes - 1
+      }
     }
   }
 }

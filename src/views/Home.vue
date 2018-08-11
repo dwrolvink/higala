84bc4aa586e02e2134c65e1dccf4bc4a8a48fba1
X1Zeth2X <zethiscool11@gmail.com>
2018-08-11 03:31:11 +0000

    Updates and changes to work with the backend.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,9 +10,19 @@
       </v-flex>
 
       <v-flex xs6>
-        <div id="posts" v-for="(post, index) in posts" :key="index">
-          <!-- Testing and learning. -->
-          <Post :post="post"/>
+        <div v-if="loading" align="center">
+          <v-progress-circular
+          :size="50"
+          color="deep-purple lighten-1"
+          indeterminate
+          >
+        </v-progress-circular>
+        </div>
+        <div v-else>
+          <Create/>
+          <div v-for="(post, index) in posts" :key="index">
+            <Post :post="post"/>
+          </div>
         </div>
       </v-flex>
     </v-layout>
@@ -21,19 +31,41 @@
 </template>
 
 <script>
+/* eslint-disable */
+import axios from 'axios';
 import { mapState } from 'vuex'
-import Post from '@/components/Post.vue'
+import Post from '@/components/PostStuff/Post.vue'
+import Create from '@/components/PostStuff/Create.vue'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      posts: null,
+      errors: null,
+      loading: false
+    }
+  },
   components: {
+    Create,
     Post
   },
-  computed: {
-    ...mapState([
-      'posts'
-    ])
+  created() {
+    this.getPosts()
+  },
+  methods: {
+    getPosts() {
+      this.error = this.posts = null
+      this.loading = true
+      axios.get('http://127.0.0.1:5000/posts')
+      .then(response => {
+        this.posts = response.data
+        this.loading = false
+        })
+      .catch(e => {
+        this.errors.push(e);
+      });
+    }
   }
-}
+};
 </script>
-

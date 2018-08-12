4e0d402408fa51f76523e108e534b0da0c5486c0
X1Zeth2X <zethiscool11@gmail.com>
2018-08-12 14:21:39 +0400

    Changed loggedIn to null at initial.

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     backendUrl: "http://127.0.0.1:5000/",
-    loggedIn: false
+    loggedIn: null
   },
   getters: {},
   mutations: {},

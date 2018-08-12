3197304eefa12ee430e113468e84bdb914168356
X1Zeth2X <zethiscool11@gmail.com>
2018-08-12 14:21:21 +0400

    Added Signup page.

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Login from "./views/Login.vue";
+import Signup from "./views/Signup.vue";
 
 Vue.use(Router);
 
@@ -18,13 +19,9 @@ export default new Router({
       component: Login
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/signup",
+      name: "signup",
+      component: Signup
     },
     {
       path: "*",

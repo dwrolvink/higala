009a9dc308af57cddc02a588b7d7dca0fadcb015
X1Zeth2X <zethiscool11@gmail.com>
2018-08-12 17:53:53 +0400

    Removed and cleaned up.

--- src/store.js
+++ src/store.js
@@ -5,8 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    backendUrl: "http://127.0.0.1:5000/",
-    loggedIn: null
+    backendUrl: "http://127.0.0.1:5000/"
   },
   getters: {},
   mutations: {},

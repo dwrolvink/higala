4813cb6bbc59d8ef5246b037911fb41e1bd973dc
X1Zeth2X <zethiscool11@gmail.com>
2018-08-14 14:54:48 +0400

    Removed backendUrl.. :P

--- src/store.js
+++ src/store.js
@@ -4,9 +4,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    backendUrl: "http://127.0.0.1:5000/"
-  },
+  state: {},
   getters: {},
   mutations: {},
   actions: {}

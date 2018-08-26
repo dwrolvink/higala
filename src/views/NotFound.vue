9fed8d85d15857048f347897672a9e0eb528e752
X1Zeth2X <zethiscool11@gmail.com>
2018-08-26 12:46:35 +0000

    Added 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="hero is-dark is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <b-icon 
+          icon="gentoo" 
+          size="is-large"
+          type="is-black"
+        ></b-icon>
+        <span class="is-size-2 has-text-grey">Linux boi not found</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+}
+</script>

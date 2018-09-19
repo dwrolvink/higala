cc526d31b1cd79f9040f34c3ab16f35c471067de
X1Zeth2X <zethiscool11@gmail.com>
2018-09-19 13:37:59 +0000

    Experimental image uploading and displaying.

--- src/components/PostStuff/Create.vue
+++ src/components/PostStuff/Create.vue
@@ -32,10 +32,11 @@
         >Post</button>
       </div>
       <div class="column is-2">
-        <button class="button is-info is-fullwidth is-rounded"
+        <button 
+          :class="['button', 'is-fullwidth', 'is-rounded', image_id ? 'is-success': 'is-info']"
           @click="imageUploadModal = true"
         >
-          <b-icon icon="image"></b-icon>
+          <b-icon :icon="image_id ? 'check': 'image'"></b-icon>
         </button>
       </div>
     </div>
@@ -49,44 +50,45 @@
           </p>
         </header>
         <div class="card-content">
-          <div class="columns is-centered is-mobile">
-            <div class="column is-half">
-              <b-field type="is-danger">
-                <b-upload
-                  drag-drop
-                  v-model="imageFile"
-                >
-                  <section class="section">
-                    <div class="content has-text-centered">
-                      <p>
-                        <b-icon
-                          icon="upload"
-                          size="is-large"
-                        >
-                        </b-icon>
-                      </p>
-                      <p>
-                        Drop your image here or click to upload
-                      </p>
-                    </div>
-                  </section>
-                </b-upload>
-              </b-field>
+          <div v-if="errors.has('image')" class="notification is-danger">
+            {{ errors.first('image') }}
+          </div>
 
-              <div class="tags">
-                <span v-for="(file, index) in imageFile" 
-                  :key="index"
-                  class="tag is-primary is-large"
-                >{{ file.name }}
-                  <button 
-                    class="delete is-small"
-                    type="button"
-                    @click="removeImage(index)"
-                  ></button>
-                </span>
-              </div>
+          <img :src="imageUrl">
+
+          <div class="level is-mobile">
+            <div class="level-left">
+              <button class="button is-info"
+                @click="onPickFile"
+              >
+                Select Image
+              </button>
+
+              <input 
+                v-validate="'image|size:10240|ext:png,jpg,jpeg,gif'"
+                name="image"
+                type="file" 
+                style="display: none;" 
+                ref="fileInput"
+                @change="onFilePicked"
+                accept="image/*"
+              >
+            </div>
+
+            <div class="level-right">
+              <button 
+                :class="['button', 'is-primary', uploadingImage ? 'is-loading': '']"
+                :disabled="!imageValid"
+                @click="uploadImage"
+              >
+                <b-icon
+                  icon="send"
+                >
+                </b-icon>
+              </button>
             </div>
           </div>
+
         </div>
       </div>
     </b-modal>
@@ -102,7 +104,11 @@ export default {
     return {
       postContent: "",
       imageUploadModal: false,
-      imageFile: []
+      imageUrl: "",
+      imageValid: false,
+      image: null,
+      uploadingImage: false,
+      image_id: ""
     };
   },
   computed: {
@@ -110,15 +116,13 @@ export default {
   },
   methods: {
     submit() {
-      if (this.postContent === "") {
-        this.toast("You cannot leave the field empty.", "is-warning");
-      } else {
+      if (this.postContent != "" || this.image_id != "") {
         axios
           .post(
             this.backendUrl + "/posts",
             {
               content: this.postContent,
-              image_id: null
+              image_id: this.image_id
             },
             {
               headers: {
@@ -129,6 +133,8 @@ export default {
           .then(response => {
             if (response.status === 200) {
               this.postContent = "";
+              this.image = null;
+              this.image_id = "";
               this.$emit("postCreated", response.data.new_post);
             }
           })
@@ -140,7 +146,58 @@ export default {
               );
             }
           });
+      } else {
+        this.toast("You cannot create an empty post.", "is-warning");
       }
+    },
+    onPickFile() {
+      this.$refs.fileInput.click();
+    },
+    onFilePicked(event) {
+      const files = event.target.files;
+      let imageFile = files[0];
+      if (imageFile.name.lastIndexOf(".") <= 0) {
+        this.toast("Please add a valid file!", "is-warning");
+      } else {
+        const fReader = new FileReader();
+        fReader.addEventListener("load", () => {
+          this.imageUrl = fReader.result;
+        });
+        fReader.readAsDataURL(imageFile);
+        this.imageValid = true;
+        this.image = imageFile;
+      }
+    },
+    uploadImage() {
+      this.uploadingImage = true;
+      const formData = new FormData();
+      formData.append("image", this.image);
+      axios
+        .post(this.backendUrl + "/imageupload", formData, {
+          headers: {
+            Authorization: "Bearer " + localStorage.access_token,
+            "Content-Type": "multipart/form-data"
+          }
+        })
+        .then(response => {
+          if (response.data.success === true) {
+            this.image_id = response.data.image_id;
+            this.imageUploadModal = false;
+          }
+        })
+        .catch(error => {
+          let status = error.response.status;
+          if (status === 404) {
+            this.toast("No file found!", "is-danger");
+          } else if (status === 403) {
+            this.toast("Method not allowed!", "is-danger");
+          } else if (status === 413) {
+            this.toast("File is too large! (Max 10MB)", "is-danger")
+          } else {
+            this.toast("Something went wrong during the process", "is-danger");
+          }
+        })
+        .then((this.uploadingImage = false), (this.image = null));
     },
     toast(msg, type) {
       this.$toast.open({

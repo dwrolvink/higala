f5202912f81c5bb4c11b57f6adfbf62d0fe4b484
X1Zeth2X <zethiscool11@gmail.com>
2018-10-11 11:53:12 +0000

    Removed moment-timezone and changed designs.

--- src/components/Saturday.vue
+++ src/components/Saturday.vue
@@ -17,29 +17,32 @@
       <p>
         <span class="has-text-weight-semibold">Current time</span> &rarr; {{ time.current }}
       </p>
-      <p class="mt2">
+      <p class="mt1">
         <span class="has-text-weight-semibold">Earliest Timezone (Kiribati/Kiritimati)</span> &rarr;
         {{ time.earliest }}
       </p>
-      <p class="mt2">
+      <p class="mt1">
         <span class="has-text-weight-semibold">Furthest Timezone (Baker Island Time)</span> &rarr;
         {{ time.late }}
       </p>
-      <hr/>
-      <p v-if="isItSaturday" class="has-text-weight-semibold mt2 has-text-success">
-        Yes, it's Saturday. 
-        <b-icon icon="emoticon-happy" size="is-small"></b-icon>
-      </p>
-      <p v-else class="has-text-weight-semibold mt2 has-text-danger">
-        Nope, it isn't Saturday yet.
-        <b-icon icon="emoticon-sad" size="is-small"></b-icon>
-      </p>
+    </div>
+    <div class="card-footer">
+      <div class="card-footer-item">
+        <p v-if="isItSaturday" class="has-text-weight-semibold has-text-success">
+          Yes, it's Saturday. 
+          <b-icon icon="emoticon-happy" size="is-small"></b-icon>
+        </p>
+        <p v-else class="has-text-weight-semibold mt2 has-text-danger">
+          Nope, it isn't Saturday yet.
+          <b-icon icon="emoticon-sad" size="is-small"></b-icon>
+        </p>
+      </div>
     </div>
   </b-collapse>
 </template>
 
 <script>
-import moment from "moment-timezone";
+import moment from "moment";
 
 export default {
   name: "Saturday",
@@ -58,10 +61,10 @@ export default {
       this.time.current = moment().format("dddd Do, LTS");
       this.time.earliest = moment()
         .utcOffset(+14)
-        .format("dddd Do");
+        .format("dddd Do YYYY");
       this.time.late = moment()
         .utcOffset(-12)
-        .format("dddd Do");
+        .format("dddd Do YYYY");
     },
     updateCurrentTime() {
       this.time.current = moment().format("dddd Do, LTS");

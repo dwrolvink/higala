b6b5cd614f89b045c301fca3669812cd8c69a9e1
X1Zeth2X <zethiscool11@gmail.com>
2018-10-20 22:41:28 +0000

    Added post modal, will be used later on.

---
+++ src/components/Modals/PostModal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image">
+        <img src="">
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostModal",
+  props: ["active"]
+};
+</script>

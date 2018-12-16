788ab546fc82dcd9a0d2dc36e67fe56ba8f659b3
dwrolvink <dwrolvink@gmail.com>
2018-12-16 23:16:55 +0100

    reply fix

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    backendUrl: "http://127.0.0.1:4000",
+    backendUrl: "http://80.101.63.121:4000",
     isReplyModalActive: false
   },
   mutations: {

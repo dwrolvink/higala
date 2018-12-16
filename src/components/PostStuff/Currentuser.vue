176044296e262817bf451498147cfc2bec1a37a7
Danny Dalton <daltond2@hawkmail.newpaltz.edu>
2018-12-15 23:54:54 -0500

    added null-checking to current user

--- src/components/PostStuff/Currentuser.vue
+++ src/components/PostStuff/Currentuser.vue
@@ -1,31 +1,19 @@
 <template>
   <b-collapse class="card">
     <div slot="trigger" slot-scope="props" class="card-header">
-
       <p class="card-header-title">
-        <span v-if="currentuser.first_name && currentuser.last_name">
-          {{ currentuser.first_name }} {{ currentuser.last_name }}
-        </span>
+        <span
+          v-if="currentuser.first_name && currentuser.last_name"
+        >{{ currentuser.first_name }} {{ currentuser.last_name }}</span>
         <span v-else>{{ currentuser.username }}</span>
         <span v-show="admin" class="ml2">
-          <b-tooltip 
-            label="User is an admin"
-            position="is-bottom"
-            type="is-dark"
-            square
-            animated>
-            <b-icon
-              icon="star-circle"
-              size="is-small"
-            >
-            </b-icon>
+          <b-tooltip label="User is an admin" position="is-bottom" type="is-dark" square animated>
+            <b-icon icon="star-circle" size="is-small"></b-icon>
           </b-tooltip>
         </span>
       </p>
       <a class="card-header-icon">
-        <b-icon
-            :icon="props.open ? 'menu-up' : 'menu-down'">
-        </b-icon>
+        <b-icon :icon="props.open ? 'menu-up' : 'menu-down'"></b-icon>
       </a>
     </div>
     <div class="card-content">
@@ -36,40 +24,32 @@
           <b-icon icon="format-quote-close" size="is-small"></b-icon>
         </div>
         <ul>
-          <li 
-            v-show="currentuser.first_name && currentuser.last_name"
-          ><b-icon icon="alphabetical" size="is-small"></b-icon> {{ currentuser.first_name }} {{ currentuser.last_name }}</li>
-          <li><b-icon icon="account" size="is-small"></b-icon> {{ currentuser.username }}</li>
-          <li><b-icon icon="google-circles-group" size="is-small"></b-icon> {{ joined }}</li>
+          <li v-show="currentuser.first_name && currentuser.last_name">
+            <b-icon icon="alphabetical" size="is-small"></b-icon>
+            {{ currentuser.first_name }} {{ currentuser.last_name }}
+          </li>
+          <li>
+            <b-icon icon="account" size="is-small"></b-icon>
+            {{ currentuser.username }}
+          </li>
+          <li>
+            <b-icon icon="google-circles-group" size="is-small"></b-icon>
+            {{ joined }}
+          </li>
         </ul>
       </div>
     </div>
     <footer class="card-footer">
-        <a class="card-footer-item">
-          <button 
-            class="button is-danger is-fullwidth"
-            @click="goToProfile"
-          >
-            Profile
+      <a class="card-footer-item">
+        <button class="button is-danger is-fullwidth" @click="goToProfile">Profile</button>
+        <b-tooltip position="is-bottom" label="Logout" animated square type="is-dark">
+          <button class="ml1 button is-light" @click="confirmLogout">
+            <b-icon icon="exit-to-app"></b-icon>
           </button>
-          <b-tooltip
-            position="is-bottom"
-            label="Logout"
-            animated
-            square
-            type="is-dark"
-          >
-            <button class="ml1 button is-light" @click="confirmLogout">
-              <b-icon
-                icon="exit-to-app"
-              >
-              </b-icon>
-            </button>
-          </b-tooltip>
-        </a>
+        </b-tooltip>
+      </a>
     </footer>
   </b-collapse>
-
 </template>
 
 <script>
@@ -99,8 +79,10 @@ export default {
     },
     checkRole() {
       let currentuser = this.currentuser;
-      if (currentuser.roles != null && currentuser.roles.includes(1)) {
-        this.admin = true;
+      if (currentuser != null && currentuser != undefined) {
+        if (currentuser.roles != null && currentuser.roles.includes(1)) {
+          this.admin = true;
+        }
       }
     },
     toast(msg, type) {
@@ -125,8 +107,8 @@ export default {
     confirmLogout() {
       this.$dialog.confirm({
         title: "Logging out",
-        message: "Are you sure you want to <b>logout</b> of your account?",
-        confirmText: "Logout",
+        message: "Are you sure you want to <b>log out</b> of your account?",
+        confirmText: "Log out",
         type: "is-warning",
         hasIcon: true,
         icon: "exit-to-app",
